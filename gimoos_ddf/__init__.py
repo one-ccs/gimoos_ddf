c51403c8c721cd5019799697fbe78162b6c4e92a
one-ccs <one-ccs@foxmail.com>
2025-06-06 17:52:03 +0800

    fix: 修复获取版本报错的问题

--- gimoos_ddf/__init__.py
+++ gimoos_ddf/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from importlib.metadata import version
+
 from .interface import _C4 as _C4
 from .interface import C4 as C4
 from .interface import StateChangeMode as StateChangeMode
@@ -9,6 +11,7 @@ from .interface import PersistData as PersistData
 from .logger import logger as logger
 from .create import DriverType as DriverType
 
+__version__ = version(__name__)
 
 __all__ = [
     '_C4',
